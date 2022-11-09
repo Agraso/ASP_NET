d4b0dca29a82de5c3e71a1051dda6ea910adfa16
Agraso <110435937+Agraso@users.noreply.github.com>
2022-11-09 19:50:36 -0400

    Proyecto1 de ASP.Net

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portafolio.Models;
+using Portafolio.Servicios;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        {
+            _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel { Proyectos = proyectos };
+
+            return View(modelo);
+        }
+
+        public IActionResult ProyectosALaVista()
+        {
+           var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+        }
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Servicios/RepositorioProyectos.cs
@@ -0,0 +1,50 @@
+﻿using Portafolio.Models;
+using System.Collections.Generic;
+
+namespace Portafolio.Servicios
+{
+
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() { new Proyecto
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                link = "https://www.amazon.com/",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+
+            new Proyecto
+            {
+                Titulo = "New York Times",
+                Descripcion = "Página de noticias en React",
+                link = "https://www.nytimes.com/",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+
+            new Proyecto
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red Social para compartir en comunidades",
+                link = "https://www.reddit.com/",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+
+            new Proyecto
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en línea para comprar videojuegos",
+                link = "https://store.steampowered.com/?l=spanish",
+                ImagenURL = "/imagenes/steam.png"
+            },
+            };
+        }
+
+    }
+}
