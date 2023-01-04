97ae5b7254ab618c0ee8f7c6ebfccdb199eab7ee
Agraso <110435937+Agraso@users.noreply.github.com>
2023-01-04 18:15:52 -0400

    Add files via upload

---
+++ Models/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Portafolio.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Proyecto> Proyectos { get; set; }
+    }
+}
